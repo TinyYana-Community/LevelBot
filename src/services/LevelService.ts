4bc52e1e7e76e07f77ced6591329256972988266
夜花櫻 <yanasakuranight@gmail.com>
2023-05-31 22:26:27 +0800

    add async await and addExp add checkMemberData

--- src/services/LevelService.ts
+++ src/services/LevelService.ts
@@ -1,4 +1,3 @@
-import Discord from 'discord.js';
 import Member from '../models/Member';
 import * as fs from 'fs';
 
@@ -6,34 +5,35 @@ const dataFilePath = '../data/memberData.json';
 const memberData: Member = JSON.parse(fs.readFileSync(dataFilePath, 'utf-8'));
 
 class LevelService {
-    addExp(id: string, exp: number): void {
+    async addExp(id: string, exp: number): Promise<void> {
+        await this.checkMemberData(id);
         if (memberData[id]) {
             memberData[id].experience += exp;
             if (this.canLevelUp(id)) {
-                this.addLevel(id, 1);
+                await this.addLevel(id, 1);
             }
-            this.saveMemberData(memberData);
+            await this.saveMemberData(memberData);
         }
     }
 
-    addLevel(id: string, levels: number): void {
+    async addLevel(id: string, levels: number): Promise<void> {
         if (memberData[id]) {
             memberData[id].level += levels;
-            this.saveMemberData(memberData);
+            await this.saveMemberData(memberData);
         }
     }
 
-    addCurrency(id: string, amount: number) {
+    async addCurrency(id: string, amount: number): Promise<void> {
         if (memberData[id]) {
             memberData[id].currency += amount;
-            this.saveMemberData(memberData);
+            await this.saveMemberData(memberData);
         }
     }
 
-    spendCurrency(id: string, amount: number) {
+    async spendCurrency(id: string, amount: number): Promise<boolean> {
         if (memberData[id] && memberData[id].currency >= amount) {
             memberData[id].currency -= amount;
-            this.saveMemberData(memberData);
+            await this.saveMemberData(memberData);
             return true; // 表示消費成功
         } else {
             return false; // 表示消費失敗
@@ -48,21 +48,21 @@ class LevelService {
         }
     }
 
-    setCurrency(id: string, amount: number) {
+    async setCurrency(id: string, amount: number): Promise<void> {
         if (memberData[id]) {
             memberData[id].currency = amount;
-            this.saveMemberData(memberData);
+            await this.saveMemberData(memberData);
         }
     }
 
-    checkMemberData(id: string): void {
+    async checkMemberData(id: string): Promise<void> {
         if (!memberData[id]) { // 如果沒有成員資料
             memberData[id] = { // 創建一個新資料
                 level: 1, // 初始等級為1
                 experience: 0, // 初始經驗值為0
                 currency: 0 // 初始貨幣數量為0
             };
-            this.saveMemberData(memberData); // 保存成員資料
+            await this.saveMemberData(memberData); // 保存成員資料
         }
     }
 
@@ -80,27 +80,27 @@ class LevelService {
         return currentExp < requiredExp && currentLevel > 1; // 如果經驗值低於所需值且等級高於1，則可以降級
     }
 
-    reduceExp(id: string, exp: number): void {
+    async reduceExp(id: string, exp: number): Promise<void> {
         if (memberData[id]) {
             memberData[id].experience -= exp; // 減少經驗值
             if (this.canLevelDown(id)) { // 檢查是否可以降級
-                this.reduceLevel(id, 1); // 降低一個等級
+                await this.reduceLevel(id, 1); // 降低一個等級
             }
-            this.saveMemberData(memberData);
+            await this.saveMemberData(memberData);
         }
     }
 
-    reduceLevel(id: string, levels: number): void {
+    async reduceLevel(id: string, levels: number): Promise<void> {
         if (memberData[id]) {
             memberData[id].level -= levels; // 減少等級
             if (memberData[id].level < 1) { // 如果等級低於1
                 memberData[id].level = 1; // 將等級設為1
             }
-            this.saveMemberData(memberData);
+            await this.saveMemberData(memberData);
         }
     }
 
-    saveMemberData(memberData: Member): void {
+    async saveMemberData(memberData: Member): Promise<void> {
         try {
             const data = JSON.stringify(memberData, null, 2);
             fs.writeFileSync(dataFilePath, data, 'utf-8');
