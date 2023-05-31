243d6c74413001537b573c62904dc4eb83d8f6d9
夜花櫻 <yanasakuranight@gmail.com>
2023-06-01 01:46:54 +0800

    User lookup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import { Client, GatewayIntentBits } from "discord.js";
 import * as fs from "fs";
 import LevelService from "./services/LevelService";
+import Config from "./models/Config";
+import UserInfo from "./commands/UserInfo";
+
+const config: Config = JSON.parse(fs.readFileSync('config.json', {
+    encoding: 'utf8'
+}));
 
 const token: { token: string } = JSON.parse(fs.readFileSync('token.json', {
     encoding: 'utf8'
@@ -31,14 +37,22 @@ client.on('ready', () => {
     console.log("等級系統已被啟動");
 });
 
-const levelService: LevelService = new LevelService();
-
-client.on('messageCreate', (message) => {
-
+client.on('messageCreate', async (message) => {
+    if (message.author.bot) return;
+    await LevelService.addExp(message.author.id, config.exp.chatMessageExpCount);
 });
 
-client.on('messageReactionAdd', (reaction, user) => {
+client.on('messageReactionAdd', async (reaction, user) => {
+    if (user.bot) return;
+    await LevelService.addExp(user.id, config.exp.reationExpCount);
+});
 
+const userInfo = new UserInfo(client);
+
+client.on('interactionCreate', async (interaction) => {
+    if (interaction.isCommand()) {
+        await userInfo.showUserInfo(interaction);
+    }
 });
 
 client.on('error', (error) => { console.log(error) });
