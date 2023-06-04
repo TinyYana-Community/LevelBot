0b87293b8c7cfcfbc696e13e6901a44a5ad10afd
夜花櫻 <yanasakuranight@gmail.com>
2023-06-04 15:32:58 +0800

    add LOTS of js doc

--- src/services/LevelService.ts
+++ src/services/LevelService.ts
@@ -4,7 +4,16 @@ import * as fs from 'fs';
 const dataFilePath = './data/memberData.json';
 const memberData: Member = JSON.parse(fs.readFileSync(dataFilePath, 'utf-8'));
 
+/**
+ * A class that provides functions for managing member data, such as level, experience and currency.
+ */
 class LevelService {
+    /**
+     * Adds a given amount of experience to a member and checks if they can level up.
+     * @param id The ID of the member.
+     * @param exp The amount of experience to add.
+     * @returns A promise that resolves when the operation is done.
+     */
     static async addExp(id: string, exp: number): Promise<void> {
         await this.checkMemberData(id);
         if (memberData[id]) {
@@ -16,6 +25,12 @@ class LevelService {
         }
     }
 
+    /**
+     * Adds a given number of levels to a member.
+     * @param id The ID of the member.
+     * @param levels The number of levels to add.
+     * @returns A promise that resolves when the operation is done.
+     */
     static async addLevel(id: string, levels: number): Promise<void> {
         if (memberData[id]) {
             memberData[id].level += levels;
@@ -23,6 +38,12 @@ class LevelService {
         }
     }
 
+    /**
+     * Adds a given amount of currency to a member.
+     * @param id The ID of the member.
+     * @param amount The amount of currency to add.
+     * @returns A promise that resolves when the operation is done.
+     */
     static async addCurrency(id: string, amount: number): Promise<void> {
         if (memberData[id]) {
             memberData[id].currency += amount;
@@ -30,16 +51,27 @@ class LevelService {
         }
     }
 
+    /**
+     * Spends a given amount of currency from a member if they have enough.
+     * @param id The ID of the member.
+     * @param amount The amount of currency to spend.
+     * @returns A promise that resolves with a boolean indicating whether the operation was successful or not.
+     */
     static async spendCurrency(id: string, amount: number): Promise<boolean> {
         if (memberData[id] && memberData[id].currency >= amount) {
             memberData[id].currency -= amount;
             await this.saveMemberData(memberData);
-            return true; // 表示消費成功
+            return true;
         } else {
-            return false; // 表示消費失敗
+            return false;
         }
     }
 
+    /**
+     * Gets the current amount of currency of a member.
+     * @param id The ID of the member.
+     * @returns The amount of currency or 0 if the member does not exist.
+     */
     static getCurrency(id: string): number {
         if (memberData[id]) {
             return memberData[id].currency;
@@ -48,22 +80,38 @@ class LevelService {
         }
     }
 
+    /**
+     * Gets the current level of a member.
+     * @param id The ID of the member.
+     * @returns The level or 0 if the member does not exist.
+     */
     static async getLevel(id: string): Promise<number> {
         if (memberData[id]) {
-            return memberData[id].level; // 返回成員的等級
+            return memberData[id].level;
         } else {
-            return 0; // 如果沒有成員資料，返回0
+            return 0;
         }
     }
 
+    /**
+     * Gets the current experience of a member.
+     * @param id The ID of the member.
+     * @returns The experience or 0 if the member does not exist.
+     */
     static async getExperience(id: string): Promise<number> {
         if (memberData[id]) {
-            return memberData[id].experience; // 返回成員的經驗值
+            return memberData[id].experience;
         } else {
-            return 0; // 如果沒有成員資料，返回0
+            return 0;
         }
     }
 
+    /**
+     * Sets the currency of a member to a given amount.
+     * @param id The ID of the member.
+     * @param amount The amount of currency to set.
+     * @returns A promise that resolves when the operation is done.
+     */
     static async setCurrency(id: string, amount: number): Promise<void> {
         if (memberData[id]) {
             memberData[id].currency = amount;
@@ -71,6 +119,11 @@ class LevelService {
         }
     }
 
+    /**
+     * Checks if a member exists in the data and creates one if not.
+     * @param id The ID of the member.
+     * @returns A promise that resolves when the operation is done.
+     */
     static async checkMemberData(id: string): Promise<void> {
         if (!memberData[id]) { // 如果沒有成員資料
             memberData[id] = { // 創建一個新資料
@@ -82,6 +135,11 @@ class LevelService {
         }
     }
 
+    /**
+     * Checks if a member can level up based on their current level and experience.
+     * @param id The ID of the member.
+     * @returns A boolean indicating whether the member can level up or not.
+     */
     static canLevelUp(id: string): boolean {
         const currentLevel = memberData[id].level;
         const currentExp = memberData[id].experience;
@@ -89,33 +147,55 @@ class LevelService {
         return currentExp >= requiredExp && currentLevel < 50;
     }
 
+    /**
+     * Checks if a member can level down based on their current level and experience.
+     * @param id The ID of the member.
+     * @returns A boolean indicating whether the member can level down or not.
+     */
     static canLevelDown(id: string): boolean {
         const currentLevel = memberData[id].level;
         const currentExp = memberData[id].experience;
-        const requiredExp = Math.floor(450 * Math.pow(1.15, currentLevel - 2)); // 計算上一個等級所需的經驗值
-        return currentExp < requiredExp && currentLevel > 1; // 如果經驗值低於所需值且等級高於1，則可以降級
+        const requiredExp = Math.floor(450 * Math.pow(1.15, currentLevel - 2));
+        return currentExp < requiredExp && currentLevel > 1;
     }
 
+    /**
+     * Reduces a given amount of experience from a member and checks if they can level down.
+     * @param id The ID of the member.
+     * @param exp The amount of experience to reduce.
+     * @returns A promise that resolves when the operation is done.
+     */
     static async reduceExp(id: string, exp: number): Promise<void> {
         if (memberData[id]) {
-            memberData[id].experience -= exp; // 減少經驗值
-            if (this.canLevelDown(id)) { // 檢查是否可以降級
-                await this.reduceLevel(id, 1); // 降低一個等級
+            memberData[id].experience -= exp;
+            if (this.canLevelDown(id)) {
+                await this.reduceLevel(id, 1);
             }
             await this.saveMemberData(memberData);
         }
     }
 
+    /**
+     * Reduces a given number of levels from a member and sets their level to 1 if it goes below that.
+     * @param id The ID of the member.
+     * @param levels The number of levels to reduce.
+     * @returns A promise that resolves when the operation is done.
+     */
     static async reduceLevel(id: string, levels: number): Promise<void> {
         if (memberData[id]) {
-            memberData[id].level -= levels; // 減少等級
-            if (memberData[id].level < 1) { // 如果等級低於1
-                memberData[id].level = 1; // 將等級設為1
+            memberData[id].level -= levels;
+            if (memberData[id].level < 1) {
+                memberData[id].level = 1;
             }
             await this.saveMemberData(memberData);
         }
     }
 
+    /**
+     * Saves the member data to a JSON file.
+     * @param memberData The member data object to save.
+     * @returns A promise that resolves when the operation is done or rejects with an error if it fails.
+     */
     static async saveMemberData(memberData: Member): Promise<void> {
         try {
             const data = JSON.stringify(memberData, null, 2);
